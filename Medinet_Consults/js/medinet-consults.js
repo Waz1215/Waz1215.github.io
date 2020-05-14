88b875c14c019124e1cfb8a3f00d4aeae338450b
Wazir Rafeek <war256@nyu.edu>
2020-05-14 01:17:05 -0400

    Added FAQs

--- Medinet_Consults/js/medinet-consults.js
+++ Medinet_Consults/js/medinet-consults.js
@@ -4,9 +4,22 @@ $( document ).ready(function() {
 	$("#main-nav .nav-link.scroll").click(function(event) {
     	event.preventDefault();
     	$(".navbar-toggler").click();
-    	var pos = $(this)[0].hash;
+    	var el = $(this).data("destination");
     	$('html, body').animate({
-        	scrollTop: $(pos).offset().top-50
+        	scrollTop: $(el).offset().top-50
     	}, 500, "swing");
+	});
+
+
+	//Flip Arrow Button for Collapsible FAQs
+	$(".question-col a").click(function(event) {
+    	if ($(this).hasClass("unclicked")){
+    		$(this).removeClass("unclicked");
+    		$(this).addClass("clicked");
+    	}
+    	else{
+    		$(this).removeClass("clicked");
+    		$(this).addClass("unclicked");
+    	}
 	});
 });
