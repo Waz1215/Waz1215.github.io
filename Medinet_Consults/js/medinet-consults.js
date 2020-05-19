eab9a57b0dc1209a22cf13ec3349799ccd85d079
Wazir Rafeek <war256@nyu.edu>
2020-05-18 22:41:57 -0400

    Body changed REVERT

--- Medinet_Consults/js/medinet-consults.js
+++ Medinet_Consults/js/medinet-consults.js
@@ -29,12 +29,12 @@ $( document ).ready(function() {
     	if ($("#navbarNavAltMarkup").hasClass("slide-out")){
     		$("#navbarNavAltMarkup").removeClass("slide-out");
     		$("#navbarNavAltMarkup").addClass("slide-in");
-    		$("body").css("overflow-y", "hidden");
+    		// $("body").css("overflow-y", "hidden");
     	}
     	else{
     		$("#navbarNavAltMarkup").removeClass("slide-in");
     		$("#navbarNavAltMarkup").addClass("slide-out");
-    		$("body").css("overflow-y", "auto");
+    		// $("body").css("overflow-y", "auto");
     	}
 	});
 });
