942ce8e056927ac90e8eb51b9f04c52370d23a68
Wazir Rafeek <war256@nyu.edu>
2020-10-29 21:30:39 -0400

    Clean Slate

--- js/interactive.js
+++
@@ -1,9 +0,0 @@
-var options = {
-    paths: 'path',     // Shape we want to draw
-    pointsNumber: 5,            // Number of points
-    maxDistance: 10,             // Max distance among points
-    color: '#5C1523',   // Element to move accordingly with the centroid of the shape
-    intensity: 0.7,
-    debug: true               // Uncomment this to see the points
-};
-var myJellyCircle = new Jelly('.jelly-circle-canvas', options);
