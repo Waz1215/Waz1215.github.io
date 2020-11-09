65f772b1ed65816591bef6419e0f0a668e1a6868
Wazir Rafeek <war256@nyu.edu>
2020-11-09 00:56:50 -0500

    implemented videos

--- MyPractice/js/custom-carousel.js
+++ MyPractice/js/custom-carousel.js
@@ -63,7 +63,67 @@ function customCarouselPartner(){
 	});
 }
 
+var videoIndex = 3;
+function cycleVideos(){
+	var override = false;
+
+	$(".feature-section").click(function(){ 
+		override = true;
+		ids[videoIndex].css("transition-duration",  0 + "s");
+		sections[videoIndex].removeClass("active");
+    	sections[videoIndex].addClass("inactive");
+    	ids[videoIndex].css("width",  "0%");
+		videoIndex = $(this).data('index');
+		video.children[0].src = `img/videos/${sources[videoIndex]}.mp4`;
+    	video.load();
+	});
+
+	var sources = ["Scripts",  "Referrals", "Bookings", "News" ];
+	var ids = [$("#access .progress-bar"), $("#referrals .progress-bar"), $("#bookings .progress-bar"), $("#news .progress-bar") ];
+	var sections = [$("#access"), $("#referrals"), $("#bookings"), $("#news") ];
+	var video = $("#iPhoneDemo")[0];
+	var length = 12;
+
+	video.load();
+	video.onloadedmetadata = function() {
+
+	  //override = false;
+	  length = video.duration;
+	  video.play();
+	  sections[videoIndex].removeClass("inactive");
+	  sections[videoIndex].addClass("active");
+	  ids[videoIndex].css("transition-duration",  length + "s");
+	  ids[videoIndex].css("width",  "100%");
+	  timeout(length);
+	};
+
+	var timeout = function(l){
+		
+	    	setTimeout(function () {
+	    		if (override) {
+	    			override = false;
+	    			return;
+	    		}
+	    		console.log("OVERRIDE ", override);
+	    		ids[videoIndex].css("transition-duration",  0 + "s");
+	    		sections[videoIndex].removeClass("active");
+	    		sections[videoIndex].addClass("inactive");
+	    		ids[videoIndex].css("width",  "0%");
+	    		videoIndex = videoIndex == 3 ? 0 : videoIndex + 1;
+	    		video.children[0].src = `img/videos/${sources[videoIndex]}.mp4`;
+	    		video.load();
+	    	}, l * 1000);			
+	}
+	timeout();
+
+}
+	
+	// setTimeout(function(){console.log("DONE")}, video[0].duration * 1000);
+
+
+
 
 $(document).ready(function(){
 	customCarouselPartner();
+	cycleVideos();
 });
